807f83f03c945fbd086bb50ba1a0f07158bfe671
Tsunoyaka <mousakun@gmail.com>
2023-06-28 22:28:08 +0600

    media

--- config/urls.py
+++ config/urls.py
@@ -45,6 +45,4 @@ urlpatterns = [
     path('api/route/', include('apps.route.urls')),
     path('api/transfer/', include('apps.transfer.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
