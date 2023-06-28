65de3106b558a5a947a77e2ad948d9b5db771bd7
Tsunoyaka <mousakun@gmail.com>
2023-06-28 14:49:55 +0600

    api

--- config/urls.py
+++ config/urls.py
@@ -38,7 +38,7 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('auto/', include('apps.auto.urls')),
     path('review/', include('apps.review.urls')),
